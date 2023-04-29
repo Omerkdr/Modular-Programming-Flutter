9cc806d6609c2747be57a05f454f68749fe3881c
Ömer Kadir <omerkdr8@outlook.com>
2023-04-30 01:54:14 +0300

    loginAuth

--- test/login/login_test.dart
+++ test/login/login_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_full_learn/404/bloc/feature/login/model/login_model.dart';
-import 'package:flutter_full_learn/404/bloc/feature/login/service/login_service.dart';
-import 'package:flutter_full_learn/product/service/vexana_network_manager.dart';
+import 'package:veli_capraz/404/bloc/feature/login/model/login_model.dart';
+import 'package:veli_capraz/404/bloc/feature/login/service/login_service.dart';
+import 'package:veli_capraz/product/service/vexana_network_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -9,7 +9,8 @@ void main() {
     loginService = LoginService(VexanaNetworkManager());
   });
   test('Login Test - eve.holt@reqres.in & cityslicka ', () async {
-    final response = await loginService.login(LoginModel('eve.holt@reqres.in', 'cityslicka'));
+    final response = await loginService
+        .login(LoginModel('eve.holt@reqres.in', 'cityslicka'));
     expect(response?.data, isNotNull);
   });
 }
